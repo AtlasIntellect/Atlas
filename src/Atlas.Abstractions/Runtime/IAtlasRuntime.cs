073604dfe9cb54491dfe3245acd92e8cc7e5c299
Pietervo <pietervanoorschot@hotmail.com>
2026-08-11 16:50:27 +0200

    Establish Atlas runtime abstraction

---
+++ src/Atlas.Abstractions/Runtime/IAtlasRuntime.cs
@@ -0,0 +1,9 @@
+﻿namespace Atlas.Abstractions.Runtime;
+
+/// <summary>
+/// Defines the interface for the Atlas runtime, which provides core functionality and services for the Atlas framework.
+/// </summary>
+public interface IAtlasRuntime
+{
+    
+}
---
+++ src/Atlas.Core/Runtime/AtlasRuntime.cs
@@ -0,0 +1,9 @@
+﻿using Atlas.Abstractions.Runtime;
+
+namespace Atlas.Core.Runtime;
+
+/// <inheritdoc/>
+public sealed class AtlasRuntime : IAtlasRuntime
+{
+    
+}
