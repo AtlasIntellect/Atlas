9ff384cac1f47a2f8c423e4e610db36c1a3ffef0
Pietervo <pietervanoorschot@hotmail.com>
2026-08-11 16:42:46 +0200

    Introduce Atlas event abstraction

---
+++ src/Atlas.Abstractions/Events/AtlasEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace Atlas.Abstractions.Events;
+
+/// <summary>
+/// Represents an event that can be published and subscribed to within the Atlas framework.
+/// </summary>
+public abstract record AtlasEvent
+{
+    /// <summary>
+    /// Gets the unique identifier of the event.
+    /// </summary>
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    /// <summary>
+    /// Gets the timestamp indicating when the event occurred.
+    /// </summary>
+    public DateTimeOffset OccuredAt { get; init; } = DateTimeOffset.UtcNow;
+}
