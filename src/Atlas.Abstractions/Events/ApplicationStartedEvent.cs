b4b9e3bcdaff9d9b101e074585495ba7cf338a78
Pietervo <pietervanoorschot@hotmail.com>
2026-08-11 16:45:17 +0200

    Add application started event

---
+++ src/Atlas.Abstractions/Events/ApplicationStartedEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace Atlas.Abstractions.Events;
+
+/// <summary>
+/// Represents an event that is published when the application has started.
+/// </summary>
+public sealed record ApplicationStartedEvent : AtlasEvent;
