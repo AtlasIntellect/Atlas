d1518f1e693c46d938acbf61703f4382addd85b3
Pietervo <pietervanoorschot@hotmail.com>
2026-08-13 13:58:28 +0200

    Implement in-memory event dispatcher

--- src/Atlas.Core/Events/AtlasEventDispatcher.cs
+++ src/Atlas.Core/Events/AtlasEventDispatcher.cs
@@ -7,9 +7,38 @@ namespace Atlas.Core.Events;
 /// </summary>
 public sealed class AtlasEventDispatcher : IAtlasEventDispatcher
 {
-    /// <inheritdoc/>
-    public Task PublishAsync(AtlasEvent @event, CancellationToken cancellationToken = default)
+    private readonly Dictionary<Type, List<Func<AtlasEvent, CancellationToken, Task>>> _handlers = [];
+
+    /// <summary>
+    /// Registers an event handler for a specific event type.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event.</typeparam>
+    /// <param name="handler">The event handler to register.</param>
+    public void RegisterHandler<TEvent>(IAtlasEventHandler<TEvent> handler) where TEvent : AtlasEvent
     {
-        throw new NotImplementedException();
+        var eventType = typeof(TEvent);
+
+        if (!_handlers.TryGetValue(eventType, out var handlers))
+        {
+            handlers = [];
+            _handlers[eventType] = handlers;
+        }
+
+        handlers.Add((@event, cancellationToken) =>
+            handler.HandleAsync((TEvent)@event, cancellationToken));
+    }
+
+    /// <inheritdoc/>
+    public async Task PublishAsync(AtlasEvent @event, CancellationToken cancellationToken = default)
+    {
+        if (!_handlers.TryGetValue(@event.GetType(), out var handlers))
+            return;
+
+        foreach (var handler in handlers)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            
+            await handler(@event, cancellationToken);
+        }
     }
 }
